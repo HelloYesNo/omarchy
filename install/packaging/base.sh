08c698449aeb586ef2518391ee4b55a612f00a2b
HelloYesNo <38365632+HelloYesNo@users.noreply.github.com>
2025-10-28 20:22:12 +0200

    Change dnf5 command to skip broken packages

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,3 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.packages" | grep -v '^$')
-dnf5 install -y --skip-unavailable --allowerasing "${packages[@]}"
+dnf5 install -y --skip-unavailable --skip-broken --allowerasing "${packages[@]}"
