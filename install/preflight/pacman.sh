aabc337082c49730c6513c895014eda933724fc7
HelloYesNo <38365632+HelloYesNo@users.noreply.github.com>
2025-10-28 15:07:56 +0200

    Replace pacman commands with dnf5 equivalents

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -1,11 +1,7 @@
 if [[ -n ${OMARCHY_ONLINE_INSTALL:-} ]]; then
   # Install build tools
-  sudo pacman -S --needed --noconfirm base-devel
-
-  # Configure pacman
-  sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
-  sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
+  sudo dnf5 install @c-development @development-tools
 
   # Refresh all repos
-  sudo pacman -Syu --noconfirm
+  sudo dnf5 update
 fi
