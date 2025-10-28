2d1f71da084cad5c4d62ff472fb2aec247010db5
HelloYesNo <38365632+HelloYesNo@users.noreply.github.com>
2025-10-28 19:27:40 +0200

    Update pacman.sh

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -1,6 +1,5 @@
+# Install build tools
+dnf5 install @c-development @development-tools
 
-  # Install build tools
-  sudo dnf5 install @c-development @development-tools
-
-  # Refresh all repos
-  sudo dnf5 update
+# Refresh all repos
+dnf5 update
