f9510a8dcf202047f087bcbedcf5a7944759b6e9
HelloYesNo <38365632+HelloYesNo@users.noreply.github.com>
2025-10-28 19:28:07 +0200

    Update base.sh

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,3 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.packages" | grep -v '^$')
-sudo dnf5 install -y --skip-unavailable --allowerasing "${packages[@]}"
+dnf5 install -y --skip-unavailable --allowerasing "${packages[@]}"
